cb64840fff60235aa3ebc2fd38e07b41595dbfb1
kcruz-216 <kirstiemadelienecruzmangalus@gmail.com>
2021-11-09 12:39:10 -0800

    initial commit for Angular parts 456

--- kirstie-cruz-angular-projects/udemy-app-project/udemy-my-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ kirstie-cruz-angular-projects/udemy-app-project/udemy-my-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,9 +7,10 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe ('A Test Recipe', 'This is simply a test', 'https://assets.bonappetit.com/photos/615dc5b7a99b3f199e10ce6a/1:1/w_1200%2Cc_limit/Adobo-Mushroom-Tacos-Alt.jpg'),
-    new Recipe ('A Test Recipe', 'This is simply a test', 'https://assets.bonappetit.com/photos/615dc5b7a99b3f199e10ce6a/1:1/w_1200%2Cc_limit/Adobo-Mushroom-Tacos-Alt.jpg')  
+    new Recipe ('Another Test Recipe', 'This is simply a test', 'https://assets.bonappetit.com/photos/615dc5b7a99b3f199e10ce6a/1:1/w_1200%2Cc_limit/Adobo-Mushroom-Tacos-Alt.jpg')  
   ];
   
   constructor() { }
@@ -17,4 +18,7 @@ export class RecipeListComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
 }
